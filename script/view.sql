67de018e3d2ee02659b6c64ce14acefe56f59926
FitiaLalaina <119358772+TsikyAro@users.noreply.github.com>
2024-05-04 23:48:59 +0300

    view2

--- script/view.sql
+++ script/view.sql
@@ -1,21 +1,21 @@
-create view dernier_utilisateur_niveau as 
-    SELECT id, idutilisateur, idniveau, idcourssuivant
+create or replace view dernier_utilisateur_niveau as 
+    SELECT id, idutilisateur, idniveau, idcourstermine
     FROM (
-        SELECT id, idutilisateur, idniveau, idcourssuivant,
+        SELECT id, idutilisateur, idniveau, idcourstermine,
             ROW_NUMBER() OVER (PARTITION BY idutilisateur ORDER BY id DESC) AS row_num
         FROM utilisateur_niveau
     ) sub
     WHERE row_num = 1;
 
 create or replace view cours_utilisateur as 
-    select ordre,titre,dn.idcourssuivant,dn.idutilisateur from cours cross join dernier_utilisateur_niveau dn ;
+    select ordre,titre,dn.idcourstermine,dn.idutilisateur from cours cross join dernier_utilisateur_niveau dn ;
 
 
 create or replace view cours_utilisateur_indice as
-select ordre,titre,idcourssuivant,idutilisateur,
+select ordre,titre,idcourstermine,idutilisateur,
     CASE 
-        WHEN ordre >= idcourssuivant THEN '10'
-        WHEN ordre = idcourssuivant-1 THEN '5'
-        WHEN ordre < idcourssuivant-1 THEN '0'
+        WHEN ordre > idcourstermine+1 THEN '10'
+        WHEN ordre = idcourstermine+1 THEN '5'
+        WHEN ordre < idcourstermine+1 THEN '0'
     END as indice
 from cours_utilisateur;
