21b27cd17be2b833f4c3fbf2a06ad5b7a0c7b438
Finaritra Andriantahiana <fyandriantahiana@gmail.com>
2024-05-05 05:23:27 +0300

    Page fiche politesse

--- application/views/pages/cours/politesse.php
+++ application/views/pages/cours/politesse.php
@@ -1,71 +1,84 @@
 <div class="politesse">
-    <br />
     <div class="content-hero p-relative d-flex align-items-center text-center flex-column">
-
         <h1 class="title mb-30 text-transform-upper dsn-load-animate">
             COURTESY
         </h1>
         <p class="subtitle p-relative line-shap dsn-load-animate">
-            <span class="pl-10 pr-10 background-section">Say hello/just stop by</span>
+            <span class="pl-10 pr-10 background-section">Say hello/Just stop by</span>
         </p>
     </div>
-    <br />
-    <div class="container ">
+    <br/><br/>
+    <div class="container">
         <div class="image-response">
             <div class="dsn-posts d-grid grid-lg-3 grid-sm-2 our-blog our-blog-classic our-blog-full-img">
-
-                <div class=" blog-item p-relative d-flex align-items-center h-100 w-100"
-                    data-swiper-parallax-scale="0.85">
+                <div class="blog-item p-relative d-flex align-items-center" data-swiper-parallax-scale="0.85">
                     <div class="box-content p-relative">
-
                         <div class="box-content-body">
                             <h4 class="title-block mb-20">
                                 <p class="effect-ajax">Say hello</p>
                             </h4>
                             <div class="content-img-fiche">
-                                <img class="img-fiche"
-                                    src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-                                    data-dsn-src="<?php echo base_url() ?>assets/clients/assets/img/fiche/salutation.jpg"
-                                    alt="">
+                                <img class="img-fiche" src="<?php echo base_url() ?>assets/clients/assets/img/fiche/salutation.jpg" alt="" style="width:300px;height:300px;object-fit:cover">
                             </div>
-                            <br/>
+                            <br />
                             <p>It is a sign of respect and courtesy towards others.</p>
                         </div>
                     </div>
                 </div>
-                <div class=" blog-item p-relative d-flex align-items-center h-100 w-100"
-                    data-swiper-parallax-scale="0.85">
-                    <div class="box-meta">
-                        <div class="entry-date">
-                            <span class="author">Dsn Grid</span>
-                            <p class="effect-ajax">March , 17th
-                                2020</p>
-                        </div>
-                    </div>
+                <div class="blog-item p-relative d-flex align-items-center h-100 w-100" data-swiper-parallax-scale="0.85">
                     <div class="box-content p-relative">
-
                         <div class="box-content-body">
-                            <div class="metas">
-                                <span>Creative</span>
-                                <span>Travel</span>
-                            </div>
                             <h4 class="title-block mb-20">
-                                <p class="effect-ajax">The Day I Lost
-                                    My
-                                    Child
-                                    in
-                                    Charles de Gaulle Airport</p>
+                                <p class="effect-ajax">Just stop by</p>
                             </h4>
                             <div class="content-img-fiche">
-                                <img class="img-fiche"
-                                    src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-                                    data-dsn-src="<?php echo base_url() ?>assets/clients/assets/img/fiche/salutation.jpg"
-                                    alt="">
+                                <img class="img-fiche" src="<?php echo base_url() ?>assets/clients/assets/img/fiche/saynothing.jpg" alt="" style="width:300px;height:300px;object-fit:cover">
                             </div>
-                            <br/>
-                            <p>Streamer fish California halibut Pacific saury. Slickhead
-                                grunion
-                                lake trout. Canthigaster rostrata spikefish…</p>
+                            <br />
+                            <p>This can make you seem rude or disinterested.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br/><br/>
+
+    <div class="content-hero p-relative d-flex align-items-center text-center flex-column">
+        <p class="subtitle p-relative line-shap dsn-load-animate">
+            <span class="pl-10 pr-10 background-section">Say thank you/Ignore</span>
+        </p>
+    </div>
+
+    <br/><br/>
+    <div class="container">
+        <div class="image-response">
+            <div class="dsn-posts d-grid grid-lg-3 grid-sm-2 our-blog our-blog-classic our-blog-full-img">
+                <div class="blog-item p-relative d-flex align-items-center" data-swiper-parallax-scale="0.85">
+                    <div class="box-content p-relative">
+                        <div class="box-content-body">
+                            <h4 class="title-block mb-20">
+                                <p class="effect-ajax">Say thank you</p>
+                            </h4>
+                            <div class="content-img-fiche">
+                                <img class="img-fiche" src="<?php echo base_url() ?>assets/clients/assets/img/fiche/grateful.jpg" alt="" style="width:300px;height:300px;object-fit:cover">
+                            </div>
+                            <br />
+                            <p>It shows your appreciation for what someone has done for you.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="blog-item p-relative d-flex align-items-center h-100 w-100" data-swiper-parallax-scale="0.85">
+                    <div class="box-content p-relative">
+                        <div class="box-content-body">
+                            <h4 class="title-block mb-20">
+                                <p class="effect-ajax">Ignore</p>
+                            </h4>
+                            <div class="content-img-fiche">
+                                <img class="img-fiche" src="<?php echo base_url() ?>assets/clients/assets/img/fiche/ignore.jpg" alt="" style="width:300px;height:300px;object-fit:cover">
+                            </div>
+                            <br />
+                            <p>Cela peut donner l'impression que vous prenez les choses pour acquises ou que vous n'êtes pas reconnaissant.</p>
                         </div>
                     </div>
                 </div>
@@ -73,4 +86,51 @@
         </div>
     </div>
 
+    <br/><br/>
+    <div class="content-hero p-relative d-flex align-items-center text-center flex-column">
+        <p class="subtitle p-relative line-shap dsn-load-animate">
+            <span class="pl-10 pr-10 background-section">Ask politely/Ask impatiently</span>
+        </p>
+    </div>
+
+    <br/><br/>
+    <div class="container">
+        <div class="image-response">
+            <div class="dsn-posts d-grid grid-lg-3 grid-sm-2 our-blog our-blog-classic our-blog-full-img">
+                <div class="blog-item p-relative d-flex align-items-center" data-swiper-parallax-scale="0.85">
+                    <div class="box-content p-relative">
+                        <div class="box-content-body">
+                            <h4 class="title-block mb-20">
+                                <p class="effect-ajax">Ask politely</p>
+                            </h4>
+                            <div class="content-img-fiche">
+                                <img class="img-fiche" src="<?php echo base_url() ?>assets/clients/assets/img/fiche/ask.jpg" alt="" style="width:300px;height:300px;object-fit:cover">
+                            </div>
+                            <br />
+                            <p>This shows that you respect the other person's feelings and needs.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="blog-item p-relative d-flex align-items-center h-100 w-100" data-swiper-parallax-scale="0.85">
+                    <div class="box-content p-relative">
+                        <div class="box-content-body">
+                            <h4 class="title-block mb-20">
+                                <p class="effect-ajax">Ask impatiently</p>
+                            </h4>
+                            <div class="content-img-fiche">
+                                <img class="img-fiche" src="<?php echo base_url() ?>assets/clients/assets/img/fiche/speed.jpg" alt="" style="width:300px;height:300px;object-fit:cover">
+                            </div>
+                            <br />
+                            <p>This can make you seem selfish or insensitive to the needs of others.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+    <br/><br/>
+
+<br/><br/>
 </div>
